388dafae9f95fcae2c75fc35a23a0d342b48b761
dneog <debashisneog23@gmail.com>
2023-04-22 12:39:54 +0530

    reusable Input component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../Context/auth-Context';
 
 const Home = (props) => {
+  const AuthCtx= useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>LogOut</Button>
+      <Button onClick={AuthCtx.onLogout}>LogOut</Button>
     </Card>
   );
 };
